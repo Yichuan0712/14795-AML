920a385493fdca399795224260be6f7b5f55b510
Yichuan <1758783038@qq.com>
2024-10-21 11:42:19 -0700

    Commit and Push

--- preprocessor/preprocessor.py
+++ preprocessor/preprocessor.py
@@ -2,10 +2,10 @@ import pandas as pd
 from imblearn.over_sampling import SMOTE
 
 
-def preprocess(file_path, apply_smote=False):
+def preprocess(file_path, n, random_state, apply_smote=False):
     # Load the data
     df = pd.read_csv(file_path)
-    df = df.sample(n=200000, random_state=1)  # Sample 'n' rows from the data
+    df = df.sample(n=n, random_state=random_state)  # Sample 'n' rows from the data
     df = df.drop_duplicates(keep='first')  # Remove duplicates
 
     # Split features and target
@@ -34,31 +34,24 @@ def preprocess(file_path, apply_smote=False):
     ]
     X = pd.get_dummies(X, columns=categorical_columns)
 
-    # Apply SMOTE only if training data
     if apply_smote:
         smote = SMOTE(sampling_strategy=0.3, random_state=42)
         X, y = smote.fit_resample(X, y)
 
-    # Create a new DataFrame from the data
     data = pd.DataFrame(X, columns=X.columns)
     data['Is_laundering'] = y
 
-    # After applying get_dummies, check the columns
-    # print(X.columns)  # This will help you see what columns were generated
     high_risk_countries = {"Nigeria", "Morocco", "Turkey", "Pakistan", "Mexico", "Spain"}
-    # Identify one-hot encoded columns for high-risk countries
     sender_columns = [f"Sender_bank_location_{country}" for country in high_risk_countries]
     receiver_columns = [f"Receiver_bank_location_{country}" for country in high_risk_countries]
     data['high_risk_countries'] = data[sender_columns + receiver_columns].sum(axis=1).apply(lambda x: 1 if x > 0 else 0)
 
-    # Convert boolean columns to integers
     bool_columns = data.select_dtypes(include=['bool', 'object']).columns
     for col in bool_columns:
         if data[col].dtype == 'object':
             data[col] = data[col].map({'True': True, 'False': False, 'true': True, 'false': False})
         data[col] = data[col].astype(int)
 
-    # Handle any missing values in the target column
     if data['Is_laundering'].isna().sum() > 0:
         data = data.dropna(subset=['Is_laundering'])
 
