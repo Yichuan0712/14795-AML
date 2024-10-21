4fc6dd1740e0c310015bf4842510d904be7a275b
Yichuan <1758783038@qq.com>
2024-10-21 11:11:23 -0700

    Commit and Push

--- preprocessor/preprocessor.py
+++ preprocessor/preprocessor.py
@@ -43,22 +43,22 @@ def preprocess(file_path, apply_smote=False):
     data = pd.DataFrame(X, columns=X.columns)
     data['Is_laundering'] = y
 
-    # Add high-risk country feature
-    high_risk_countries = {"Nigeria", "Morocco", "Turkey", "Pakistan", "Mexico", "Spain"}
-    sender_columns = [f"Sender_bank_location_{country}" for country in high_risk_countries]
-    receiver_columns = [f"Receiver_bank_location_{country}" for country in high_risk_countries]
-
-    data['high_risk_countries'] = data[sender_columns + receiver_columns].sum(axis=1).apply(lambda x: 1 if x > 0 else 0)
-
-    # Convert boolean columns to integers
-    bool_columns = data.select_dtypes(include=['bool', 'object']).columns
-    for col in bool_columns:
-        if data[col].dtype == 'object':
-            data[col] = data[col].map({'True': True, 'False': False, 'true': True, 'false': False})
-        data[col] = data[col].astype(int)
-
-    # Handle any missing values in the target column
-    if data['Is_laundering'].isna().sum() > 0:
-        data = data.dropna(subset=['Is_laundering'])
+    # # Add high-risk country feature
+    # high_risk_countries = {"Nigeria", "Morocco", "Turkey", "Pakistan", "Mexico", "Spain"}
+    # sender_columns = [f"Sender_bank_location_{country}" for country in high_risk_countries]
+    # receiver_columns = [f"Receiver_bank_location_{country}" for country in high_risk_countries]
+    #
+    # data['high_risk_countries'] = data[sender_columns + receiver_columns].sum(axis=1).apply(lambda x: 1 if x > 0 else 0)
+    #
+    # # Convert boolean columns to integers
+    # bool_columns = data.select_dtypes(include=['bool', 'object']).columns
+    # for col in bool_columns:
+    #     if data[col].dtype == 'object':
+    #         data[col] = data[col].map({'True': True, 'False': False, 'true': True, 'false': False})
+    #     data[col] = data[col].astype(int)
+    #
+    # # Handle any missing values in the target column
+    # if data['Is_laundering'].isna().sum() > 0:
+    #     data = data.dropna(subset=['Is_laundering'])
 
     return data
